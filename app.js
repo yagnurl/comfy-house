ac8651f2ae42e848c2140baf2651f74873b42ec3
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-09-06 17:30:17 +0300

    the logic implementation and saving in local storage

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".product-center");
-
+const productsDOM = document.querySelector(".products-center");
 // cart
 let cart = [];
+
+//buttons
+let buttonsDOM = [];
 
 // getting the product
 class Products {
@@ -35,17 +37,82 @@ class Products {
 // display product
 class UI {
   displayProducts(products) {
-    console.log(products);
+    let result = "";
+    products.forEach((product) => {
+      result += `
+        <article class="product">
+            <div class="img-container">
+                <img class="product-img" src=${product.image}>
+                <button class="bag-btn" data-id=${product.id}>
+                <i class="fas fa-shopping-cart"></i>
+                add to bag
+                </button>
+            </div>
+            <h3>${product.title}</h3>
+            <h4>${product.price}</h4>
+        </article>
+      `;
+    });
+    productsDOM.innerHTML = result;
+  }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
+      if (inCart) {
+        button.innerText = "IN CART";
+        button.disabled = true;
+      }
+      button.addEventListener("click", (event) => {
+        event.target.innerText = "IN CART";
+        event.target.disabled = true;
+
+        // get product from products
+        // let cardItem = Storage.getProducts(id);
+        let cartItem = { ...Storage.getProducts(id), amount: 1 }; // we convert Storage.getProducts(id) into an object and added 1 more property (key) named amount and set the value 1
+
+        // add product to the cart
+        cart = [...cart, cartItem];
+
+        // save cart in local storge
+        Storage.saveCart(cart);
+        // set cart values
+        // display cart item
+        // show the cart
+      });
+    });
   }
 }
 
 // local storage
-class Storage {}
+class Storage {
+  static saveProduct(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  static getProducts(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find((product) => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
 
   // get all products
-  products.getProducts().then((products) => ui.displayProducts(products));
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProduct(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
